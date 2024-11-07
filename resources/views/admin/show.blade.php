bbc9902e1f444584229e6579841057cc3716695f
FrancescoMinichiello <fraps2278@gmail.com>
2024-11-07 17:56:17 +0100

    General fix

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -2,19 +2,24 @@
 
 @section('page-title', "{{ $project->name }}")
 
-@section('main-content')
+@section('content')
     <h1 class="text-center text-white mb-4">{{ $project['name'] }}</h1>
     <div class="container">
         <div class="row mb-2">
             <div>
                 <div class="card-body">
                     <h1 class="card-text">{{ $project['name'] }}</h1>
-                    <p class="card-text"> <a href="{{ $project['link'] }}">Link:</a> </p>
+                    <p class="card-text"> <a href="{{ $project['link'] }}">
+                            <h4> Vai al progetto</h4>
+                        </a> </p>
                 </div>
             </div>
-            <a class="btn btn-secondary mb-2" href="{{ route('admin.index') }}">
-                Torna indietro
-            </a>
+            <div class="col-4 my-5">
+
+                <a class="btn btn-primary mb-2" href="{{ route('admin.index') }}">
+                    <h2 class="m-0"> Torna indietro</h2>
+                </a>
+            </div>
         </div>
     </div>
 @endsection
