a969f45b250476ea796e7d83f744d7117b642a02
FrancescoMinichiello <fraps2278@gmail.com>
2024-11-07 16:05:07 +0100

    Added fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class project extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "name",
+        "link",
+        "types_id"
+    ];
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,5 +12,27 @@ class ProjectSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void {}
+    public function run(): void
+    {
+        $fullstack = Type::where("name", "Full-stack")->first();
+        $backend = Type::where("name", "Back-end")->first();
+        $frontend = Type::where("name", "Front-end")->first();
+
+
+        Project::create([
+            "name" => "Progetto Laravel full-stack",
+            "link" => "https://github.com/FrancescoMinichiello/laravel-many-to-many",
+            "type_id" => $fullstack->id,
+        ]);
+        Project::create([
+            "name" => "Progetto Laravel back-end",
+            "link" => "https://github.com/FrancescoMinichiello/laravel-primi-passi",
+            "type_id" => $backend->id,
+        ]);
+        Project::create([
+            "name" => "Progetto front-end",
+            "link" => "https://github.com/FrancescoMinichiello/Playstation",
+            "type_id" => $frontend->id,
+        ]);
+    }
 }
